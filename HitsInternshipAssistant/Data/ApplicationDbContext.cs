7cba313a233fab980909b2ef3e2de7a39a6cda42
Alexander Dashuk <pinygod@mail.ru>
2022-04-10 16:56:23 +0700

    119: Project with auth added.

---
+++ HitsInternshipAssistant/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HitsInternshipAssistant.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HitsInternshipAssistant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ HitsInternshipAssistant/Data/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿namespace HitsInternshipAssistant.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
