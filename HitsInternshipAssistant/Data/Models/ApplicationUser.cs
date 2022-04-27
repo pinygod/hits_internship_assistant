b5697847a7e09b0e4ac3ad8cf1d729080dc151e5
Alexander Dashuk <pinygod@mail.ru>
2022-04-27 23:11:13 +0700

    196: CRUD Резюме.

--- HitsInternshipAssistant/Data/Models/ApplicationUser.cs
+++ HitsInternshipAssistant/Data/Models/ApplicationUser.cs
@@ -8,6 +8,8 @@
         public string LastName { get; set; }
         public Guid? CompanyId { get; set; }
         public Company? Company { get; set; }
+        public Guid? CVId { get; set; }
+        public CV CV { get; set; }
         public virtual ICollection<Vacancy> VacanciesApplied { get; set; }
     }
 }
---
+++ HitsInternshipAssistant/Data/ViewModels/EditCVViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HitsInternshipAssistant.Data.Models;
+
+namespace HitsInternshipAssistant.Data.ViewModels
+{
+    public class EditCVViewModel
+    {
+        public string AdditionalInfo { get; set; }
+        public string Contacts { get; set; }
+        public ICollection<StudentWorkDirection> WorkDirections { get; set; }
+    }
+}
