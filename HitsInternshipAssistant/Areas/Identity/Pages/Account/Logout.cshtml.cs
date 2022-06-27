58e5d0f8b3281e73f928d245c59e3c5f8122de95
NaraMara <mrjogurt18@gmail.com>
2022-06-28 01:28:21 +0700

    last minute panic fixes

--- HitsInternshipAssistant/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ HitsInternshipAssistant/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -28,16 +28,16 @@ namespace HitsInternshipAssistant.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+            //if (returnUrl != null)
+            //{
+            //    return LocalRedirect(returnUrl);
+            //}
+            //else
+            //{
+            //    // This needs to be a redirect so that the browser performs a new
+            //    // request and the identity for the user gets updated.
+            return RedirectToPage();
+            //}
         }
     }
 }
